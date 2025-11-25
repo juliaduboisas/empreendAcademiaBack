0a675e618e86a5d0f212d787e462e54b1831ec13
Júlia Du Bois <jujubadubois@gmail.com>
2025-11-25 18:00:03 -0300

    feat: GET API's for different database-related statistics added

--- databaseInfo.py
+++ databaseInfo.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fastapi import HTTPException, File
 
+# STUDENT RELATED
 def getStudent(id: int):
     try:
         df = pd.read_csv("database.csv")
@@ -48,6 +49,7 @@ def getStudentsByUnit(unit: str):
         print(f"An unexpected error occurred during CSV reading: {e}")
         raise HTTPException(status_code=500, detail="Error processing data file.")
 
+# DATABASE RELATED
 async def saveFile(file: File()):
     filePath = "database.csv"
     contents = await file.read()
@@ -59,4 +61,175 @@ async def saveFile(file: File()):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    return
+
+# STATISTICS
+def getTotalOfStudents():
+    try:
+        df = pd.read_csv("database.csv")
+        totalStudents = df.shape[0]
+
+        return totalStudents
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+
+    return
+
+def getObjetiveMode():
+    try:
+        df = pd.read_csv("database.csv")
+        mode = df["Objetivo"].mode()
+
+        return mode
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+
+    return
+
+def getTotalOfStudentsPerUnit(unit: str):
+    try:
+        df = pd.read_csv("database.csv")
+        studentsInUnit = df.loc[df["unidade"] == unit]
+        totalStudents = studentsInUnit.shape[0]
+
+        return totalStudents
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+
+    return
+
+def getStudentsMedianAge():
+    try:
+        df = pd.read_csv("database.csv")
+        medianAge = df["Idade"].median()
+
+        return medianAge
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def getMedianTimeAsStudent():
+    try:
+        df = pd.read_csv("database.csv")
+        medianTimeAsStudent = df["TempoComoAluno"].median()
+
+        return medianTimeAsStudent
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def getMedianAmountOfCheckInsPerWeek(week: int):
+    try:
+        df = pd.read_csv("database.csv")
+        column_name = f"CheckinsSemana_{week}"
+        checkInSum = df[column_name].sum()
+        totalStudents = getTotalOfStudents()
+
+        medianCheckins = float(checkInSum) / float(totalStudents)
+
+        return medianCheckins
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def getMedianAmountOfCheckIns():
+    try:
+        df = pd.read_csv("database.csv")
+        medianCheckins = float(0)
+        for i in range(1, 13):
+            medianCheckins += getMedianAmountOfCheckInsPerWeek(i)
+
+        return medianCheckins/float(12)
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def meanWeightVariation():
+    try:
+        df = pd.read_csv("database.csv")
+        weightVariationSum = df["VariacaoPeso_2m"].sum()
+        totalStudents = getTotalOfStudents()
+
+        meanWeightVariation = float(weightVariationSum) / float(totalStudents)
+
+        return meanWeightVariation
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def meanFatVariation():
+    try:
+        df = pd.read_csv("database.csv")
+        fatVariationSum = df["VariacaoGordura_2m"].sum()
+        totalStudents = getTotalOfStudents()
+
+        meanFatVariation = float(fatVariationSum) / float(totalStudents)
+
+        return meanFatVariation
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
+    return
+
+def meanLoadVariation():
+    try:
+        df = pd.read_csv("database.csv")
+        loadVariationSum = df["VariacaoCarga_2m"].sum()
+        totalStudents = getTotalOfStudents()
+
+        meanLoadVariation = float(loadVariationSum) / float(totalStudents)
+
+        return meanLoadVariation
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Data file not found on the server.")
+    except pd.errors.EmptyDataError:
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred during CSV reading: {e}")
+        raise HTTPException(status_code=500, detail="Error processing data file.")
     return
--- main.py
+++ main.py
@@ -19,10 +19,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# GENERAL REQUESTS
 @app.get("/get-gym-name")
 async def getGymName():
     return "AcadeMais"
 
+# DATABASE REQUESTS
 @app.get("/get-student-info")
 async def getStudentInfo(id: int):
     data = databaseInfo.getStudent(id);
@@ -41,6 +43,86 @@ async def getStudents():
 
     return data
 
+@app.get("/get-statistics/number-of-students")
+async def getNumberOfStudents():
+    data = databaseInfo.getTotalOfStudents()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+
+    return data
+
+@app.get("/get-statistics/objetive-mode")
+async def getMostCommonObjective():
+    data = databaseInfo.getObjetiveMode()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+
+    return data
+
+@app.get("/get-statistics/number-of-students-per-unit")
+async def getNumberOfStudentsPerUnit(unit: str):
+    data = databaseInfo.getTotalOfStudentsPerUnit(unit)
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+
+    return data
+
+
+@app.get("/get-statistics/median-age")
+async def getMedianAge():
+    data = databaseInfo.getStudentsMedianAge()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+@app.get("/get-statistics/median-time-as-student")
+async def getMedianTimeAsStudent():
+    data = databaseInfo.getMedianTimeAsStudent()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+@app.get("/get-statistics/median-checkins")
+async def getMedianCheckins(week: int):
+    if week == 0 or week > 12:
+        data = databaseInfo.getMedianAmountOfCheckIns()
+    else:
+        data = databaseInfo.getMedianAmountOfCheckInsPerWeek(week)
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+@app.get("/get-statistics/mean-weight-variation")
+async def getMeanWeightVariation():
+    data = databaseInfo.meanWeightVariation()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+@app.get("/get-statistics/mean-fat-variation")
+async def getMeanFatVariation():
+    data = databaseInfo.meanFatVariation()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+@app.get("/get-statistics/mean-load-variation")
+async def getMeanLoadVariation():
+    data = databaseInfo.meanLoadVariation()
+
+    if data is None:
+        raise HTTPException(status_code=404, detail=f"Students not found.")
+    return data
+
+
 @app.get("/get-students-by-unit")
 async def getStudentsByUnit(unit: str):
     data = databaseInfo.getStudentsByUnit(unit)
@@ -50,6 +132,7 @@ async def getStudentsByUnit(unit: str):
 
     return data
 
+# AI-RELATED REQUESTS
 @app.get("/get-students-predictions")
 async def getStudentsPredictions():
     data = iaInfo.getIndividualResults()
